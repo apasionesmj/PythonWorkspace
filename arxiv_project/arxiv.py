370665cc2a649d72be2823398b3ab99e6380ee0c
JaehyunMah <apasionesmj@naver.com>
2021-02-04 00:54:17 +0900

    0203home

--- arxiv_project/arxiv.py
+++ arxiv_project/arxiv.py
@@ -25,13 +25,28 @@ def extract_scholar(last_page):
     for result in results:
         title = result.find("p",{"class":"title"}).text.strip()
         authors = result.find_all("p",{"class":"authors"})
-        dic = {title : [authors]}
-
-
+        authors_str = ""
         for result1 in authors:
             result2 = result1.find_all("a")
             for author in result2:
-                print("[title] : " +title+ "   [authors] : " +author.text)
+                authors_str += author.text + ', '
+        abstract = result.find("p",{"class":"abstract"})
+        # more를 누르면 abstract-short의 display가 활성화
+        # less를 누르면 abstract-full의 display가 활성화 
+        abstract_span = abstract.find("span",{"class":"abstract-full"})
+        if abstract_span is not None:
+            abstract = (str(abstract_span.text.strip()))
+        else:
+            abstract = (str(abstract.string))
+        print("[Title] : " + title + "   [Authors] : " + authors_str + "    [Abstract] : " + abstract.text)
+    return scholar
+
+
+        # for result1 in authors:
+        #     result2 = result1.find_all("a")
+        #     for author in result2:
+        #         print("[title] : " +title+ "   [authors] : " +author.text)
+
     # for result1 in results:
     #     authors = result1.find_all("p",{"class":"authors"})
     #     for result2 in authors:
@@ -40,4 +55,9 @@ def extract_scholar(last_page):
     #             print(realauthor.text)
 
 
-    return scholar
+# val authors_str = ""
+# for result1 in authors:
+#             result2 = result1.find_all("a")
+#             for author in result2:
+#       authors_str += author.text
+# print("논문명 : " + title + "저자들 : " + author_str)
